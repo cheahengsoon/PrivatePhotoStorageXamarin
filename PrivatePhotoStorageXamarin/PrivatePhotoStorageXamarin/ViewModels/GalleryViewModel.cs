24cbaa43ac87d11ff4c34d280d1330489ed269f3
dimgrek <dimgrek@gmail.com>
2015-10-16 17:05:51 +0300

    MVVM implemented

---
+++ PrivatePhotoStorageXamarin/PrivatePhotoStorageXamarin/ViewModels/GalleryViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using PrivatePhotoStorageXamarin.Annotations;
+using Xamarin.Forms;
+using XLabs.Platform.Services.Media;
+
+namespace PrivatePhotoStorageXamarin.ViewModels
+{
+    public class GalleryViewModel:INotifyPropertyChanged
+    {
+        private ImageSource _image;
+        private IMediaPicker _mediaPicker = DependencyService.Get<IMediaPicker>();
+
+
+        public GalleryViewModel()
+        {
+              AddPictureCommand = new Command(AddPicture);   
+        }
+
+        public ICommand AddPictureCommand { get; private set; }
+
+        public ImageSource Image
+        {
+            get { return _image; }
+            set { _image = value; OnPropertyChanged(); }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        async void AddPicture()
+        {
+            MediaFile file = null;
+            if (_mediaPicker.IsCameraAvailable)
+            {
+                file = await _mediaPicker.TakePhotoAsync(new CameraMediaStorageOptions());
+            }
+            else
+            {
+                file = await _mediaPicker.SelectPhotoAsync(new CameraMediaStorageOptions());
+            }
+            Image = ImageSource.FromStream(() => file.Source);
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- PrivatePhotoStorageXamarin/PrivatePhotoStorageXamarin/Views/MainPage.xaml.cs
+++ PrivatePhotoStorageXamarin/PrivatePhotoStorageXamarin/Views/MainPage.xaml.cs
@@ -1,51 +1,17 @@
-﻿using System;
+﻿using PrivatePhotoStorageXamarin.ViewModels;
 using Xamarin.Forms;
-using XLabs.Platform.Services.Media;
 
 namespace PrivatePhotoStorageXamarin.Views
 {
     public partial class MainPage : ContentPage
     {
-        IMediaPicker _mediaPicker = DependencyService.Get<IMediaPicker>();
+        private GalleryViewModel _vm;
 
         public MainPage()
         {
             InitializeComponent();
-            takePhoto.Clicked += TakePhotoClicked;
-            choosePhoto.Clicked += SelectPicture;
-        }
-
-        async void TakePhotoClicked(object sender, EventArgs e)
-        {
-            MediaFile file = null;
-            if (_mediaPicker.IsCameraAvailable)
-            {
-                file = await _mediaPicker.TakePhotoAsync(new CameraMediaStorageOptions());
-            }
-            else
-            {
-                file = await _mediaPicker.SelectPhotoAsync(new CameraMediaStorageOptions());
-            }
-            previewImage.Source = ImageSource.FromStream(() => file.Source);
-        }
-
-        async void SelectPicture(object sender, EventArgs e)
-        {
-            MediaFile file = null;
-            try
-            {
-                file = await _mediaPicker.SelectPhotoAsync(new CameraMediaStorageOptions
-                {
-                    DefaultCamera = CameraDevice.Front,
-                    MaxPixelDimension = 400
-                });
-                previewImage.Source = ImageSource.FromStream(() => file.Source);
-            }
-            catch (Exception ex)
-            {
-
-            }
-
+            _vm = new GalleryViewModel();
+            BindingContext = _vm;
         }
     }
 }
